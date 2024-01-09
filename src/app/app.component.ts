6fe22d1cb9c27b90adc94e8c240cec9e4f1301cc
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2024-01-09 02:12:04 -0500

    removing useless code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import { NavComponent } from "./components/nav/nav.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { LogsContainerComponent } from "./components/logs-container/logs-container.component";
 import { LoggingService } from "(src)/app/services/logging.service";
-import { map, Observable, startWith, Subscription } from "rxjs";
+import { map, Observable, startWith } from "rxjs";
 import { MessageLog } from "(src)/app/core/mesage-log";
 
 @Component({
@@ -21,10 +21,8 @@ import { MessageLog } from "(src)/app/core/mesage-log";
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss"
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
 	title = "smartia-logging-ng";
-	private messagesSubscription!: Subscription;
-	// public logs: MessageLog[] = [];
 	public logs$!: Observable<MessageLog[]>;
 
 	constructor(private webSocketService: LoggingService) {}
@@ -34,22 +32,5 @@ export class AppComponent implements OnInit, OnDestroy {
 			map((msg) => msg.data["logs"] || []),
 			startWith([])
 		);
-		// this.messagesSubscription = this.webSocketService.incomingMessage$.subscribe({
-		// 	next: (msg) => {
-		// 		// console.info(msg);
-		// 		this.logs = msg.data["logs"];
-		// 		console.info(this.logs?.length);
-		// 	},
-		// 	error: (error) => {
-		// 		console.error(error);
-		// 	},
-		// 	complete: () => {}
-		// });
-	}
-
-	ngOnDestroy(): void {
-		// if (this.messagesSubscription) {
-		// 	this.messagesSubscription.unsubscribe();
-		// }
 	}
 }
